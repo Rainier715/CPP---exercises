86e51d2035da8e8472ddb97ac2112b40367712da
Rainier715 <146302663+Rainier715@users.noreply.github.com>
2023-09-28 10:41:51 +0800

    Add files via upload

---
+++ act8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	string lala;
+	
+	cout<<"Enter your name: ";
+	cin>>lala; //para makuha ang input
+	
+	cout<<"The legth of your name is: "<<lala.size();
+	
+	
+	
+	
+	return 0;
+}
